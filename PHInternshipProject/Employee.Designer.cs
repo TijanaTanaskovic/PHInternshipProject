3bb529e5d51472142384300768e32f55a0b523db
TijanaTanaskovic <tijana.kg98@gmail.com>
2023-03-10 18:58:20 +0100

    display of the employees and tasks

---
+++ PHInternshipProject/Employee.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace PHInternshipProject
+{
+    partial class Employee
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.employeeView = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.taskView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeView)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.taskView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1076, 650);
+            this.tabControl1.TabIndex = 2;
+          
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.employeeView);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1068, 617);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "employees";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(332, 36);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "List of all the employees";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // employeeView
+            // 
+            this.employeeView.AllowUserToOrderColumns = true;
+            this.employeeView.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.employeeView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.employeeView.Location = new System.Drawing.Point(8, 39);
+            this.employeeView.Name = "employeeView";
+            this.employeeView.RowHeadersWidth = 51;
+            this.employeeView.RowTemplate.Height = 29;
+            this.employeeView.Size = new System.Drawing.Size(1057, 209);
+            this.employeeView.TabIndex = 2;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.taskView);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1068, 617);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tasks";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // taskView
+            // 
+            this.taskView.AllowUserToOrderColumns = true;
+            this.taskView.BackgroundColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.taskView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.taskView.Location = new System.Drawing.Point(8, 39);
+            this.taskView.Name = "taskView";
+            this.taskView.RowHeadersWidth = 51;
+            this.taskView.RowTemplate.Height = 29;
+            this.taskView.Size = new System.Drawing.Size(1057, 188);
+            this.taskView.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "List of all the tasks";
+            // 
+            // Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1088, 636);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Employee";
+            this.Text = "PHCompany";
+            this.Load += new System.EventHandler(this.Employee_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeView)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.taskView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView employeeView;
+        private System.Windows.Forms.DataGridView taskView;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PHInternshipProject/Employee.cs
@@ -0,0 +1,73 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PHInternshipProject
+{
+    public partial class Employee : Form
+    {
+        public Employee()
+        {
+            InitializeComponent();
+            ShowAllEmployee();
+            ShowAllTasks();
+            //employeeView.AutoResizeColumns();
+            //taskView.AutoResizeColumns();
+
+        }
+
+        static string MySQLConnectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=phinternship; SSL Mode = None";
+        MySqlConnection databaseConnection = new MySqlConnection(Employee.MySQLConnectionString);
+        MySqlCommand cmd = new MySqlCommand();
+        MySqlDataAdapter da = new MySqlDataAdapter();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ShowAllEmployee(string searchQuery = "SELECT employee_ID, first_name, last_name, email, phone_number, date_of_birth, monthly_salary FROM employee WHERE 1=1")
+        {
+            this.databaseConnection.Open();
+            da = new MySqlDataAdapter(searchQuery, databaseConnection);
+            MySqlCommandBuilder Builder = new MySqlCommandBuilder(da);
+            var ds = new DataSet();
+            da.Fill(ds);
+            employeeView.DataSource = ds.Tables[0];
+            employeeView.AutoResizeColumns();
+            employeeView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            this.databaseConnection.Close();
+        }
+
+        public void ShowAllTasks(string searchQuery = "SELECT * FROM task")
+        {
+            this.databaseConnection.Open();
+            da = new MySqlDataAdapter(searchQuery, databaseConnection);
+            MySqlCommandBuilder Builder = new MySqlCommandBuilder(da);
+            var ds = new DataSet();
+            da.Fill(ds);
+            taskView.DataSource = ds.Tables[0];
+            taskView.AutoResizeColumns();
+            taskView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            this.databaseConnection.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Employee_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
